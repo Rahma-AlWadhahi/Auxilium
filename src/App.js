74acb25a317b7ddfc70512266c6f5f3c515ad1c4
Yuvrajhere <yuvrajisbest13@gmail.com>
2021-05-01 01:22:36 +0530

    Setup Redux - added /actions and /reducers folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import LandingPage from "./Components/LandingPage/";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import PrivateRoute from "./routes/PrivateRoute";
+import PublicRoute from "./routes/PublicRoute";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <LandingPage />
+        <Switch>
+          <PublicRoute component={LandingPage} path="/" exact />
+        </Switch>
       </div>
     </Router>
   );
