d66716daff69e147d1b19624f25f0f24d61ddf41
Yuvrajhere <yuvrajisbest13@gmail.com>
2021-05-01 12:52:15 +0530

    Updated home, appnavbar , added Post base

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,12 +1,14 @@
 import "./Home.css";
 import AppNavbar from "../AppNavbar";
+import Post from "../Post";
 
 const Home = () => {
   return (
     <div className="Home">
       <AppNavbar />
       <main>
-        Home
+        <Post />
+        <Post />
       </main>
     </div>
   );
